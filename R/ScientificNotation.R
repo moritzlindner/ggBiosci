bbaa510fa9ac3247f8bccacd5cf6aee483bc60d2
moritzlindner <moritz@lindnerlab.de>
2022-07-09 05:41:28 +0200

    First commit

---
+++ R/ScientificNotation.R
@@ -0,0 +1,95 @@
+#' Tools for working with scientfific notations.
+#'
+#' These functions perform conversions between floating-point numbers, engineering and scientific notations.
+#'
+#' @return \code{get_si()}, \code{exponent_to_si()}: A character vector with SI prefix(es)
+#' @return \code{si_to_exponent()}, \code{get_exp_eng}, \code{get_factor_eng}: A numeric vector
+#' @seealso \link[sitools:f2si]{sitools::f2si()}
+#' @name ScientificNotation
+#' @example
+#'
+#' get_exp_eng(1000)
+#' # And also:
+#' exponent_to_si(10000)
+#'
+#' si_to_exponent("nA")
+#' si_to_exponent("p")
+#'
+#'
+#' exponent_to_si(3)
+#' # But:
+#' exponent_to_si(2)
+#'
+#' get_exp_eng(1000)
+#' # And also:
+#' exponent_to_si(10000)
+#'
+#' get_factor_eng(1234)
+#' get_factor_eng(12345)
+#' @describeIn ScientificNotation Gets the SI prefix for a number
+#' @param val A floating-point number
+#' @export get_si
+get_si <- function(val) {
+  exponent_to_si(get_exp_eng(val))
+}
+
+#' @describeIn ScientificNotation Converts an SI prefix or full string into exponent
+#' @param letter SI prefix or full SI string
+#' @export si_to_exponent
+si_to_exponent <- function(letter) {
+  exponent <- seq(-24L, 24L, 3L)
+
+  if (letter == "\u03BC") {
+    letter = "u"
+  }
+
+  sipref <-
+    c("y",
+      "z",
+      "a",
+      "f",
+      "p",
+      "n",
+      "u",
+      "m",
+      "",
+      "k",
+      "M",
+      "G",
+      "T",
+      "P",
+      "E",
+      "Z",
+      "Y")
+
+  return(10 ^ exponent[sipref == substr(letter, 1, 1)])
+}
+
+#' @importFrom sitools f2si
+#' @describeIn ScientificNotation Converts an exponent into corresponding SI unit
+#' @param exp The exponent of a number in scientific notation
+#' @export exponent_to_si
+exponent_to_si <- function(exp) {
+  gsub("[^a-zA-Z]", "", sitools::f2si(10 ^ exp))
+}
+
+#' @describeIn ScientificNotation Gets the exponent of a number in engineering notation
+#' @param val A floating-point number
+#' @export get_exp_eng
+get_exp_eng <- function(val) {
+  decimals <- seq(-21, 21, 3)
+  decimals[which.min(abs(decimals - stats::median(as.numeric(
+    stringr::str_sub(format(val, scientific = T),
+                     (
+                       stringr::str_locate(format(val, scientific = T),
+                                           "e[\\+\\-]")
+                     )
+                     [, 2])
+  ))))]
+}
+
+#' @describeIn ScientificNotation Gets the factor before the base for a number
+#' @export get_factor_eng
+get_factor_eng <- function(val) {
+  val / 10 ^ (get_exp_eng(val))
+}
